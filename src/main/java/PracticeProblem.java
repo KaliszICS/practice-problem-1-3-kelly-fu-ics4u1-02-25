910f81cb3a3b72e798c4311dd630d03145c1778b
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-03-21 09:44:43 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,69 @@
+import java.io.*;
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static String readFile(String filename) {
+		FileReader fiRe = null;
+		int i;
+		char o;
+		String out = "";
+		try{
+			fiRe = new FileReader(filename);
 
+			while ((i = fiRe.read())!=-1){
+				o = ((char)i);
+				out = out + String.valueOf(o);
+
+			}
+		}
+		catch (Exception e){
+
+		}
+
+		finally {
+		try {
+		if (fiRe!=null){
+		fiRe.close();
+		}
+		}
+		catch (IOException e){
+
+		}
+	}
+
+	return out;
 	}
 
 	
-
+public static String backwardsReadFile(String filenames){
+	FileReader fil = null;
+	int i;
+	int len =0;
+	char c;
+	String out = "";
+	String actOut = "";
+	try {
+		fil = new FileReader(filenames);
+		while ((i = fil.read()) != -1){
+			c = ((char)i);
+			out = out +c;
+		}
+	}
+	catch (Exception e){
+	}
+	finally{
+		len = out.length();
+		for (int a = len-1; a>=0; a--){
+		actOut = actOut+out.charAt(a);
+	}
+	try{
+		if (fil != null){
+		fil.close();
+		}
+	}
+	catch (Exception e){}
+	}
+	return actOut;
+	}
 }
+
